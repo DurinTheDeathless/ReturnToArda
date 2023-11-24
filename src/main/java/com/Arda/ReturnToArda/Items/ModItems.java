98b3e818175fc295daa087d7faed9dbfb1d92f87
unknown <jay_de_haan@hotmail.com>
2023-11-24 19:35:27 +0100

    Pipeweed

---
+++ src/main/java/com/Arda/ReturnToArda/Items/ModItems.java
@@ -0,0 +1,26 @@
+package com.Arda.ReturnToArda.Items;
+
+import com.Arda.ReturnToArda.ReturnToArda;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.RegistryOps;
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredHolder;
+import net.neoforged.neoforge.registries.DeferredRegister;
+import net.neoforged.neoforge.registries.NeoForgeRegistries;
+
+import java.util.function.Supplier;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+                        DeferredRegister.create(Registries.ITEM, ReturnToArda.MOD_ID);
+
+    public static final Supplier<Item> Pipeweed =
+            ITEMS.register("pipeweed",
+                    () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+
+    }
+}
--- src/main/java/com/Arda/ReturnToArda/ReturnToArda.java
+++ src/main/java/com/Arda/ReturnToArda/ReturnToArda.java
@@ -1,6 +1,8 @@
 package com.Arda.ReturnToArda;
 
+import com.Arda.ReturnToArda.Items.ModItems;
 import com.mojang.logging.LogUtils;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.bus.api.SubscribeEvent;
@@ -16,30 +18,30 @@ import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(ReturnToArda.MOD_ID)
-public class ReturnToArda
-{
-    // Define mod id in a common place for everything to reference
+public class ReturnToArda{
     public static final String MOD_ID = "returntoarda";
-    // Directly reference a slf4j logger
-    private static final Logger LOGGER = LogUtils.getLogger();    public ReturnToArda(IEventBus modEventBus)
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public ReturnToArda(IEventBus modEventBus)
     {
+        ModItems.register(modEventBus);
+
         modEventBus.addListener(this::commonSetup);
 
         NeoForge.EVENT_BUS.register(this);
-
         modEventBus.addListener(this::addCreative);
 
         ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
     }
 
-    private void commonSetup(final FMLCommonSetupEvent event)
-    {
+    private void commonSetup(final FMLCommonSetupEvent event)    {
 
     }
 
-    // Add the example block item to the building blocks tab
-    private void addCreative(BuildCreativeModeTabContentsEvent event)
-    {
+    private void addCreative(BuildCreativeModeTabContentsEvent event)    {
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.Pipeweed.get());
+            }
         }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
